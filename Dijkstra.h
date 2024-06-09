d4be918409a800315384cfbc77a77c228043f3c4
Zak <zak@192.168.1.108>
2024-06-09 14:07:27 +0100

    initilised distances and added start station to priority queue

--- Dijkstra.h
+++ Dijkstra.h
@@ -7,7 +7,7 @@
 class Dijkstra
 {
 public:
-    static std::vector<std::string> findShortestPath(const Graph &graph, const std::string &start, const std::string &goal)
+    static std::vector<std::string> findShortestPath(const Graph &graph, const std::string &startingStation, const std::string &endStation)
     {
         // Store shortest distance from start to each node
         std::unordered_map<std::string, int> distancesToStart;
@@ -15,6 +15,18 @@ public:
         std::unordered_map<std::string, std::string> previousStation;
         // Priority Queue
         PriorityQueue pq(100);
+
+        // Initialise distances
+        for (const auto &station : graph.getAllStations())
+        {
+            distancesToStart[station] = INT_MAX;
+        }
+        distancesToStart[startingStation] = 0;
+
+        // Adds starting station to priority queue
+        pq.Insert(new StationDistance(startingStation, 0));
+
+        return {};
     }
 };
 
